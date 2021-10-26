fa2cd5aeef868fa3bd23241f9178f6244331e8f8
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-10-26 07:19:08 -0300

    wrapped app inside the notification context

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import { ClockContextProvider } from './hooks/useClock';
+import { NotificationProvider } from './hooks/useNotification';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ClockContextProvider>
-      <App />
-    </ClockContextProvider>
+    <NotificationProvider>
+      <ClockContextProvider>
+        <App />
+      </ClockContextProvider>
+    </NotificationProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
